132ee86e9de30b26261500d4a18e0dca6ff1d36d
Veera1011 <veera252728@gmail.com>
2025-11-10 17:16:10 +0530

    with correct

--- models/trainees.js
+++ models/trainees.js
@@ -37,7 +37,32 @@ const traineeSchema = mongoose.Schema({
   remarks: String
 }, { timestamps: true });
 
-
+// Create compound index for uniqueness
 traineeSchema.index({ employeeId: 1, trainingCode: 1, startDate: 1 }, { unique: true });
+
+// Pre-save middleware to ensure proper references
+traineeSchema.pre('save', async function(next) {
+  // Ensure employeeId is stored as string employeeId, not ObjectId
+  if (this.isModified('employeeId')) {
+    const Employee = mongoose.model('Employee');
+    const employee = await Employee.findOne({ employeeId: this.employeeId });
+    if (!employee) {
+      throw new Error('Employee not found');
+    }
+    this.employeeId = employee.employeeId;
+  }
+
+  // Ensure trainingCode is stored as string trainingCode
+  if (this.isModified('trainingCode')) {
+    const Training = mongoose.model('Training');
+    const training = await Training.findOne({ trainingCode: this.trainingCode });
+    if (!training) {
+      throw new Error('Training not found');
+    }
+    this.trainingCode = training.trainingCode;
+  }
+
+  next();
+});
 
 module.exports = mongoose.model('Trainee', traineeSchema);
